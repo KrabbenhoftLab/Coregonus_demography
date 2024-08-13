6c5860c47a7f5ee1b498c1b154ac8253b7e25f5c
njbacken <53303572+njbacken@users.noreply.github.com>
2024-08-12 20:47:47 -0400

    Update AISO_PSMC_v2_MASKED_shared_step1-2.sh
    fix masked consensus calling

--- Coregonus/AISO_PSMC_v2_MASKED_shared_step1-2.sh
+++ Coregonus/AISO_PSMC_v2_MASKED_shared_step1-2.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
 #AISO Script (ABOUT, INPUT, STEPS, OUTPUT) for running psmc analysis.
 #TJ Krabbenhoft, May 2020, University at Buffalo
-#This version run on MacPro
+
 
 #ABOUT:
 #This script takes input bam files from a genome assembly and runs PSMC analysis. The analysis requires a diploid consensus (i.e., SNPs called throughout the genome, with files created in Step 2).
 
 #INPUT:
-SEQDIR="/mnt/krab1/NJCB_data/Coregonus_PSMC_v2/Cisco-illumina-bams"
-REF="" #Reference Assembly
+
+HOME="/mnt/sdc1/Coregonus_PSMC-SMC/PSMC_masked_genome_04-10-23/"
+SEQDIR="/mnt/sdc1/Coregonus_PSMC-SMC/Cisco-illumina-genomes/"
+BWADIR=""
+REF="/mnt/sdc1/Coregonus_PSMC-SMC/PSMC_masked_genome_04-10-23/CA_GEN_1-flye_III-pilon2-purged.FINAL.chromonomer-03-21-2022.masked.fasta" #Reference Assembly
+PSMCDIR="/home/krablab/Documents/apps/psmc-master" #PSMC directory
 
 
 #STEP 1: READ MAPPING
-cd ${SEQDIR}
-
-FILELIST=`cat temp`
+cd ${HOME}
+${BWADIR}bwa index ${REF}
+FILELIST=`cat temp3`
+#FILELIST="CZ03" #This line just for running individual samples; otherwise use line above.
+for f in ${FILELIST}
+do
+${BWADIR}bwa mem -t 8 ${REF} ${SEQDIR}${f}_1.fq.gz ${SEQDIR}${f}_2.fq.gz | samtools sort -@ 8 -o ${f}.bam -O bam
+samtools index -@ 8 ${f}.bam
+done
 
 
 #STEP 2: Consenus calling:
-#for f in $FILELIST
-#do
-#bedtools intersect -v -a ./${f}_temp1.vcf.gz -b ../CA_GEN_1_assembly/CA_GEN_1.final_repeat_mask.complex.reformat_4_column.filtered.tabbed.bed -header > /mnt/krab1/NJCB_data/Coregonus_PSMC_v2/masked_vcf/${f}_temp1.masked.vcf
-#done
+
+for f in $FILELIST
+do
+bedtools intersect -v -a ./${f}_temp1.vcf.gz -b ../CA_GEN_1_assembly/CA_GEN_1.final_repeat_mask.complex.reformat_4_column.filtered.tabbed.bed -header > /mnt/krab1/NJCB_data/Coregonus_PSMC_v2/masked_vcf/${f}_temp1.masked.vcf
+done
 
 
 cd /mnt/krab1/NJCB_data/Coregonus_PSMC_v2/masked_vcf
