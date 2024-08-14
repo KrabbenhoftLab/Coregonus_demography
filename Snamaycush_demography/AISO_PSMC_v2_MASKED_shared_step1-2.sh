44722c27d2f58438e3b9c60c1c047575c0051e1b
Trevor Krabbenhoft <66832657+TrevorKrabbenhoft@users.noreply.github.com>
2024-08-14 18:50:49 -0400

    Update AISO_PSMC_v2_MASKED_shared_step1-2.sh

--- Snamaycush_demography/AISO_PSMC_v2_MASKED_shared_step1-2.sh
+++ Snamaycush_demography/AISO_PSMC_v2_MASKED_shared_step1-2.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #AISO Script (ABOUT, INPUT, STEPS, OUTPUT) for running psmc analysis.
 #TJ Krabbenhoft, May 2020, University at Buffalo
-#This version run on MacPro
 
 #ABOUT:
 #This script takes input bam files from a genome assembly and runs PSMC analysis. The analysis requires a diploid consensus (i.e., SNPs called throughout the genome, with files created in Step 2).
@@ -28,11 +27,11 @@ cd /mnt/krab1/NJCB_data/Snamaycush_PSMC_v2/masked_vcf/
 
 for f in ${FILELIST}
 do
-#echo "SNP calling completed! Things are looking up!"
+#echo "SNP calling completed."
 #set variables for math
 depth=`cat /mnt/krab1/NJCB_data/Snamaycush_PSMC_v2/avg_depth/${f}_depth.txt`
 min=`printf %.0f $(echo "$depth / 3" | bc -l)`
 max=`printf %.0f $(echo "$depth * 2" | bc -l)`
 vcfutils.pl vcf2fq -d ${min} -D ${max} ${f}_temp1.masked.vcf | gzip > ${f}.masked.diploid.fq.gz
-echo "Concensus calling completed. It won't be long now."
+echo "Consensus calling completed."
 done
